730172b7fe62945f07132b3cfbab0d5f893a02c7
JanTea <janisteayuxin@gmail.com>
2024-01-26 01:27:17 +0800

    found the difference in net profit column

--- profit_loss.py
+++ profit_loss.py
@@ -22,3 +22,17 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         # convert Day, Sales, Trading Profit, Operating Expense and Net Profit to integer data type
         profit_and_loss.append([int(row[0]),int(row[1]),int(row[2]),int(row[3]),int(row[4])]) 
 print(profit_and_loss)
+
+
+# difference in net profit column
+# use range() to start calculation from day 2 as there is no day 0 to compare with if we start from day 1
+# for loop used to compare net profit in the current day with the previous day and repeat for all days
+for i in range(len(profit_and_loss)):
+    # include a 0 for net profit difference in day 1
+    if i == 0:
+        profit_and_loss[i].append(0)
+    else:
+        difference = profit_and_loss[i][4] - profit_and_loss[i - 1][4]
+        # append differences across all days to the csv data we already have
+        profit_and_loss[i].append(difference)
+# print(profit_and_loss)
