cc98963b395a2e5ebe85e9896dbe2df94cf337e8
JanTea <janisteayuxin@gmail.com>
2024-01-26 00:49:47 +0800

    changed the file path

--- overheads.py
+++ overheads.py
@@ -4,7 +4,7 @@ import csv
 
 # create a file path to csv file
 # file we want to read and write on
-fp = Path.home()/"Janis IGP"/"csv_reports"/"overheads.csv"
+fp = Path.home()/"Project_Team C"/"csv_reports"/"overheads.csv"
 
 # read the csv file
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
