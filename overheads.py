5b8886292da2f61f7508ba45b7c90fc840e7bd25
Habir Abdat <habirabdat@gmail.com>
2024-01-26 10:47:26 +0800

    coded for the highest category and percentage

--- overheads.py
+++ overheads.py
@@ -4,7 +4,7 @@ import csv
 
 # create a file path to csv file
 # file we want to read and write on
-fp = Path.home()/"Project_Team C"/"csv_reports"/"overheads.csv"
+fp = Path.home()/"Project_Team-C"/"csv_reports"/"overheads.csv"
 
 # read the csv file
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
@@ -21,3 +21,16 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         # convert Overheads to float data type
         overheads.append([row[0],float(row[1])]) 
 # print(overheads)
+        
+# create an empty dictionary 
+ovh = {} 
+for i in overheads: 
+    ovh[i[0]] = i[1] 
+# print(ovh) 
+ 
+# find the highest overhead category 
+highest_cat = max(ovh) 
+# find the percentage for the highest overhead category 
+# access the value, which is the percentage, by inserting the key, which is the category  
+percentage = ovh[highest_cat] 
+print(f"[HIGHEST OVERHEAD] {highest_cat.upper()}: {percentage}%")
