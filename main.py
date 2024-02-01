7e142f7cf2cbda3d2c6b3fa0e666b8f8f2fd74c1
znaccc <shahanzach@gmail.com>
2024-02-01 10:43:48 +0800

    created a main function

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from pathlib import Path
 import cash_on_hand, overheads, profit_loss
+
+def main(): 
+    """  
+    - This function returns the output of all 3 financial analysis functions 
+    - Writes the results on a summary report file 
+    - No parameter required 
+    """ 
+    
