d1910f18927cc801915468da151f0900dbb78797
znaccc <shahanzach@gmail.com>
2024-02-01 10:45:33 +0800

    created variables of output functions

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ def main():
     - Writes the results on a summary report file 
     - No parameter required 
     """ 
+    # assign variables to the output of the various functions 
+    overhead_result = overheads.overhead_function() 
+    coh_result = cash_on_hand.coh_function() 
+    profitloss_result = profit_loss.profitloss_function() 
     
